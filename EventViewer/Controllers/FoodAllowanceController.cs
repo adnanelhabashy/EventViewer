550355b1beb9bf977eef95b4771ea417216b879f
adnan <adnan.elhabashy@live.com>
2018-07-29 14:25:12 +0200

    Card Modified

--- EventViewer/Controllers/FoodAllowanceController.cs
+++ EventViewer/Controllers/FoodAllowanceController.cs
@@ -24,7 +24,7 @@ namespace EventViewer.Controllers
         }
         public ActionResult CardData()
         {
-            string NodeName = "Node1";
+            string NodeName = "none";
             CardVM card = null;
             int cardNo;
             DateTime occurred;
@@ -32,53 +32,51 @@ namespace EventViewer.Controllers
             {
                 NodeName = Session["Node"].ToString();
             }
-            else
+            if (NodeName != "none")
             {
-                NodeName = "Node1";
-            }
-
-            using (IEventStoreConnection connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113)))
-            {
-                connection.ConnectAsync();
-
-                var result = Task.Run(() => connection.ReadStreamEventsBackwardAsync(NodeName, StreamPosition.End, 1, false));
-                Task.WaitAll();
-                var resultData = result.Result;
-                foreach (var Event in resultData.Events)
+                using (IEventStoreConnection connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113)))
                 {
-                    string strData = Encoding.UTF8.GetString(Event.Event.Data);
-                    string strMeta = Encoding.UTF8.GetString(Event.Event.Metadata);
-                    var datetimeConverter = new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" };
-                    dynamic Eventdata = JsonConvert.DeserializeObject(strData);
-                    dynamic EventMetadata = JsonConvert.DeserializeObject(strMeta);
-                    if (Event.Event.EventType == "checkIn" || Event.Event.EventType == "checkOut")
-                    {
-                        cardNo = (int)EventMetadata.SelectToken("CardNumber");
-                        if (cardNo != 0)
-                        {
-                            using (DB db = new DB())
-                            {
-                                card = new CardVM();
-                                var cardInfo = db.CardInfoes.Where(x => x.CardNumber == cardNo).FirstOrDefault();
-                                card.Id = cardInfo.Id;
-                                card.Name = cardInfo.Name;
-                                card.Title = cardInfo.Title;
-                                card.Department = cardInfo.Department;
-                                card.ImageName = cardInfo.ImageName;
-                                card.CardNumber = cardInfo.CardNumber;
-                            }
-                        }
-                        else
-                        {
-                            card = null;
-                        }
-                        occurred = (DateTime)Eventdata.SelectToken("occurred");
+                    connection.ConnectAsync();
 
+                    var result = Task.Run(() => connection.ReadStreamEventsBackwardAsync(NodeName, StreamPosition.End, 1, false));
+                    Task.WaitAll();
+                    var resultData = result.Result;
+                    foreach (var Event in resultData.Events)
+                    {
+                        string strData = Encoding.UTF8.GetString(Event.Event.Data);
+                        string strMeta = Encoding.UTF8.GetString(Event.Event.Metadata);
+                        var datetimeConverter = new IsoDateTimeConverter { DateTimeFormat = "dd/MM/yyyy" };
+                        dynamic Eventdata = JsonConvert.DeserializeObject(strData);
+                        dynamic EventMetadata = JsonConvert.DeserializeObject(strMeta);
+                        if (Event.Event.EventType == "checkIn" || Event.Event.EventType == "checkOut")
+                        {
+                            cardNo = (int)EventMetadata.SelectToken("CardNumber");
+                            if (cardNo != 0)
+                            {
+                                using (DB db = new DB())
+                                {
+                                    card = new CardVM();
+                                    var cardInfo = db.CardInfoes.Where(x => x.CardNumber == cardNo).FirstOrDefault();
+                                    card.Id = cardInfo.Id;
+                                    card.Name = cardInfo.Name;
+                                    card.Title = cardInfo.Title;
+                                    card.Department = cardInfo.Department;
+                                    card.ImageName = cardInfo.ImageName;
+                                    card.CardNumber = cardInfo.CardNumber;
+                                }
+                            }
+                            else
+                            {
+                                card = null;
+                            }
+                            occurred = (DateTime)Eventdata.SelectToken("occurred");
+
+                        }
                     }
                 }
             }
-
+            ViewBag.NodeName = NodeName;
             return PartialView(card);
         }
-	}
+    }
 }
