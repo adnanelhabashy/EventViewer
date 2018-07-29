2f492cd1db3fb1e0b32223cfcf9a529bb2dac41d
adnan <adnan.elhabashy@live.com>
2018-07-29 11:01:47 +0200

    Refactored and added a javascript file

---
+++ EventViewer/Scripts/SignalRAPP.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var EventHub = $.connection.eventHub;
+    $.connection.hub.start()
+                .done(function () {
+                    console.log("connected");
+                    EventHub.server.announce("connected");
+                })
+                .fail(function () { console.log("Not connected"); });
+
+    EventHub.server.announce = function (message) {
+        WriteToPage(message);
+    }
+
+    var WriteToPage = function (message) {
+        $("#Toshow").append(message + "<br />");
+    }
+})()
+
