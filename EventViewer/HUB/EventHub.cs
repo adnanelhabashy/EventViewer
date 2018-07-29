89b552478159290ecf6d076fe7c6c947cf7a923a
adnan <adnan.elhabashy@live.com>
2018-07-29 13:50:16 +0200

    Final Commit for Signal R Test

--- EventViewer/HUB/EventHub.cs
+++ EventViewer/HUB/EventHub.cs
@@ -6,16 +6,29 @@ using Microsoft.AspNet.SignalR;
 using System.Threading.Tasks;
 using EventStore.ClientAPI;
 using System.Text;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace EventViewer.HUB
 {
+    [HubName("EventHub")]
     public class EventHub : Hub
     {
+        [HubMethodName("announce")]
         public void Announce(string message)
         {
             Clients.All.Announce(message);
         }
 
+        public void GetNow()
+        {
+            DateTime timenow = DateTime.Now;
+            Clients.Caller.GetDate(timenow);
+        }
+        
+        public DateTime getServerDateTime()
+        {
+            return DateTime.Now;
+        }
        
     }
 }
--- EventViewer/Scripts/SignalRAPP.js
+++ EventViewer/Scripts/SignalRAPP.js
@@ -1,18 +1,30 @@
 ﻿(function () {
-    var EventHub = $.connection.eventHub;
+    var EventHub = $.connection.EventHub;
     $.connection.hub.start()
                 .done(function () {
+                    $.connection.hub.logging = true;
                     console.log("connected");
                     EventHub.server.announce("connected");
+                    
                 })
                 .fail(function () { console.log("Not connected"); });
 
-    EventHub.server.announce = function (message) {
+    EventHub.client.announce = function (message) {
         WriteToPage(message);
-    }
-
+    };
+    
     var WriteToPage = function (message) {
         $("#Toshow").append(message + "<br />");
     }
+
+    $("#ClickMe").click(function () {
+        EventHub.server.getServerDateTime()
+                    .done(function (data) {
+                        WriteToPage(data)
+                    })
+                    .fail(function (e) {
+                        WriteToPage("Failed to get date Error :" + e);
+                    });
+    });
 })()
 
